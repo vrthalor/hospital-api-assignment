ab8df8b008dd6eeace94be0f22675b59e4827f60
vrthalor <vrthalor@gmail.com>
2023-09-14 22:35:43 +0530

    hospital api with all fetures coddingninja assignment

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,10 @@ const passport = require('passport');
 
 const status= require('../controllers/status')
 
+
+router.get('/', (req, res) =>{
+    return res.send("Welcome to hospital API Assignment")
+})
 router.use('/doctors', require('./doctors'))
 router.use('/patients', require('./patients'))
 router.get('/reports/:status',passport.authenticate('jwt',{session:false}),status.filteredReports)
